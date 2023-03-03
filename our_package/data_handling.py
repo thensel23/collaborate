150f11cd721840ea96803b0080a980e173d6ffc8
Timo Gierlich <timo@eltima-electronic.de>
2023-03-03 16:38:44 +0100

    Improved data handling file a bit
    mainly plotting and use os.path.join

--- our_package/data_handling.py
+++ our_package/data_handling.py
@@ -1,9 +1,14 @@
+"""Data import routines"""
+
+import os
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def read_in_df(filedir, filename):
+    """Load data from testfile using pandas"""
     name = "{}{}".format(filedir, filename)
     print("Reading from file {} - pandas".format(name))
     data = pd.read_csv(name, r"\s+")
@@ -11,7 +16,8 @@ def read_in_df(filedir, filename):
 
 
 def read_in_np(filedir, filename):
-    name = "{}{}".format(filedir, filename)
+    """Load data from testfile using numpy"""
+    name = os.path.join(filedir, filename)
     print("Reading from file {} - numpy".format(name))
     data = np.loadtxt(name, skiprows=1)
     data = data.T
@@ -19,7 +25,10 @@ def read_in_np(filedir, filename):
 
 
 def plot_columns(data):
+    """Plot the data for visiual inspection."""
+    fig, ax = plt.subplots(len(data) - 1, 1, sharex=True)
     for i in range(1, len(data)):
-        plt.plot(data[0], data[i])
-        plt.title("Column {}".format(i))
-        plt.show()
+        ax[i - 1].plot(data[0], data[i])
+        ax[i - 1].set_title("Column {}".format(i))
+    fig.suptitle("first visual inspection")
+    plt.show()
